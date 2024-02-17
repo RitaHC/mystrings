7417852ead15569cf45719323f261f59439d6d85
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-02-16 20:36:59 -0500

    commit

--- mystrings.go
+++ mystrings.go
@@ -1,9 +1,19 @@
 package mystrings
 
+import "strings"
+
 func Reverse(s string) string {
 	result := ""
 	for _, v := range s {
 		result = string(v) + result
 	}
 	return result
+}
+
+func addSpace(word string) string {
+	// Split the word
+	char := strings.Split(word, "")
+	// now join the characters
+	spacedWord := strings.Join(char, " ")
+	return spacedWord
 }
