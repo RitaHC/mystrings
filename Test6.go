e4b4abfeffb91b75126ca0e6b0bcd3d0f05d3984
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-02-16 20:24:14 -0500

    commit

---
+++ Test6.go
@@ -0,0 +1,11 @@
+package Test
+
+import "strings"
+
+func addSpace(word string) string {
+	// Split the word
+	char := strings.Split(word, "")
+	// now join the characters
+	spacedWord := strings.Join(char, " ")
+	return spacedWord
+}
